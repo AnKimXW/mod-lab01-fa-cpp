2aeb6f22acca8c5de16a5f941dd6d1a218012da1
Анна Степанян <stepanyan.072002@gmail.com>
2025-05-05 13:28:46 +0300

    lab01

--- src/fun.cpp
+++ src/fun.cpp
@@ -3,37 +3,29 @@
 #include <string>
 #include <cctype>
 
-unsigned int faStr1(const char *str)
-{
+unsigned int faStr1(const char *str) {
     unsigned int count = 0;
     bool inWord = false;
     bool hasDigit = false;
-    while (*str)
-    {
-        if (std::isspace(*str))
-        {
-            if (inWord && !hasDigit)
-            {
+    while (*str) {
+        if (std::isspace(*str)) {
+            if (inWord && !hasDigit) {
                 ++count;
             }
             inWord = false;
             hasDigit = false;
-        } else
-        {
-            if (!inWord)
-            {
+        } else {
+            if (!inWord) {
                 inWord = true;
                 hasDigit = false;
             }
-            if (std::isdigit(*str))
-            {
+            if (std::isdigit(*str)) {
                 hasDigit = true;
             }
         }
         ++str;
     }
-    if (inWord && !hasDigit)
-    {
+    if (inWord && !hasDigit) {
         ++count;
     }
     return count;
@@ -41,42 +33,31 @@ unsigned int faStr1(const char *str)
 
 unsigned int faStr2(const char* str) {
     unsigned int count = 0;
-    while (*str)
-    {
+    while (*str) {
         // Пропускаем пробелы
-        while (*str && std::isspace(*str))
-        {
+        while (*str && std::isspace(*str)) {
             ++str;
         }
         if (!*str) break;
         bool correctWord = false;
         bool badWord = false;
         bool firstChar = true;
-        while (*str && !std::isspace(*str))
-        {
-            if (firstChar)
-            {
-                if (std::isupper(*str) && std::isalpha(*str))
-                {
+        while (*str && !std::isspace(*str)) {
+            if (firstChar) {
+                if (std::isupper(*str) && std::isalpha(*str)) {
                     correctWord = true;
-                }
-                else
-                {
+                } else {
                     badWord = true;
                 }
                 firstChar = false;
-            }
-            else
-            {
-                if (!std::islower(*str) || !std::isalpha(*str))
-                {
+            } else {
+                if (!std::islower(*str) || !std::isalpha(*str)) {
                     badWord = true;
                 }
             }
             ++str;
         }
-        if (correctWord && !badWord)
-        {
+        if (correctWord && !badWord) {
             ++count;
         }
     }
@@ -85,27 +66,22 @@ unsigned int faStr2(const char* str) {
 unsigned int faStr3(const char *str) {
     unsigned int totalLength = 0;
     unsigned int wordCount = 0;
-    while (*str)
-    {
-        while (*str && std::isspace(*str))
-        {
+    while (*str) {
+        while (*str && std::isspace(*str)) {
             ++str;
         }
         if (!*str) break;
         unsigned int wordLength = 0;
-        while (*str && !std::isspace(*str))
-        {
+        while (*str && !std::isspace(*str)) {
             ++wordLength;
             ++str;
         }
-        if (wordLength > 0)
-        {
+        if (wordLength > 0) {
             totalLength += wordLength;
             ++wordCount;
         }
     }
-    if (wordCount == 0)
-    {
+    if (wordCount == 0) {
         return 0;
     }
     return (totalLength + wordCount / 2) / wordCount;
